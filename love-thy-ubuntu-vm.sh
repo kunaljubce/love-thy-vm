b2fa2ce78b67c50662aa87b717bfd05a14cc0c50
Kunal Bhattacharya <kunal.jubce@gmail.com>
2020-11-17 00:35:08 +0530

    Added code to: 1) Autoupdate packages using apt get upgrade, if needed; 2) Autoinstall tree & curl packages. Code works fine!

--- love-thy-ubuntu-vm.sh
+++ love-thy-ubuntu-vm.sh
@@ -6,6 +6,21 @@ while getopts "r:" opt; do
     esac
 done
 
+# Check for available package upgrades and upgrade the required packages, if needed 
+checking_available_updates=`sudo apt update`
+echo ${checking_available_updates} | grep "up to date"
+if [ $? -eq 0 ]
+then
+    echo "Nothing to update, proceeding to next steps!"
+else
+    echo "Updating required packages..."
+    sudo apt -y upgrade
+fi
+
+# Install `tree` and `curl` packages
+echo "Installing tree and curl packages..."
+sudo apt install tree curl
+
 echo $resolution
 #xrandr --output Virtual1 --size 1920x1080
 display_name=`xrandr | grep "connected primary" | cut -d' ' -f1`
